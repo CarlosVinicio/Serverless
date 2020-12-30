cf64e9345cb5fcb867c5536719a77ab975b12ec0
carlos guaman <carlos@MBP-de-carlos.home>
2020-12-30 12:06:39 +0100

    Change mongo uri

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Conexion de base de datos con Mongoose 
-mongoose.connect("mongodb+srv://Carlos:88DwaybVQYLN2JV@cluster0.wwgsz.mongodb.net/Almuerzi?retryWrites=true&w=majority",
+mongoose.connect(process.env.MONGO_URI,
   { useNewUrlParser: true,
     useUnifiedTopology: true
   }
